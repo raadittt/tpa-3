52733314e3db96b61f649c9003489961589e78ae
Rafi <rafipersonalporto@gmail.com>
2022-10-13 15:19:49 +0700

    Memasukkan File TPA-3

---
+++ scriptSearch.js
@@ -0,0 +1,25 @@
+let hasilSearch = document.getElementById("search");
+
+let movieSearch = async () => {
+    let search = new URL(document.location).searchParams;
+    let cari = search.get("search");
+    let respon = await fetch(`https://api.themoviedb.org/3/search/movie/?api_key=199896c47b34ca6f0d9067faf4238b31&query=${cari}&page=1`);
+  
+    let mencariMovie = await respon.json();
+  
+    mencariMovie.results.forEach((item) => {
+      hasilSearch.innerHTML += `<div class="card" style="width: 20rem">
+          <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 id="card-title">${item.title}</h5>
+            <p class="">${item.overview}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item"><b>${item.release_date}</b></li>
+            <li class="list-group-item"><b>${item.vote_average}</b></li>
+          </ul>
+        </div>`;
+    });
+  };
+  
+  movieSearch()
